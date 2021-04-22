0f714f52d96251fbd81b7df17a2e03cef5b223d5
ntr <ntr001@gmail.com>
2021-04-22 19:41:44 +0200

    Initial version

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+-- Resource Metadata
+fx_version 'cerulean'
+games { 'gta5' }
+
+version '1.0.0'
+description 'FiveM keypress logging to console'
+repository 'https://github.com/ntr001/fivem-keys'
+author 'ntr'
+
+-- What to run
+client_script 'fivem_keys.lua'
